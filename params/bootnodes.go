faa61fc96a86045a76cd79296344ae69daf79cf3
bigjpr83 <37682865+bigjpr83@users.noreply.github.com>
2018-04-16 17:20:30 -0700

    Update bootnodes.go

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -19,11 +19,11 @@ package params
 // Ether-1 Bootnodes
 var MainnetBootnodes = []string{
 	// Ether-1 Bootnodes
-	//"enode://@45.77.184.141:30303",     //WEST COAST - USA
-	//"enode://@144.202.6.22:30303",      //EAST COAST - USA
-        //"enode://@108.160.142.71:30303",    //JAPAN
-	//"enode://@45.76.148.77:30303",      //SINGAPORE
-        //"enode://@45.76.117.164:30303",      //AUSTALIA
+	"enode://959caa70cbb602b0abe14b97f47f3c43813d1dbe8b54dd48882f22338b248752e8509c0e23b480481c550cca44c05e8df999680a8edb07d1f73ab0bf96575050@45.77.184.141:30301",     //WEST COAST - USA
+	"enode://74f65548bbee33122a9fce4165e43e9aa21919e99de6bf204e7829eb1491115659a56f8d39e7460937a6c16311616a499c1a5ae8c7b2f8aa71e5a7701c07b54b@144.202.6.22:30301",      //EAST COAST - USA
+        "enode://8a78da78bd8e347e8941c490b849168a12b67ed542141d05ec826902531c337d241c73b3f74e3ce0c984ae5fa1a0a6229f709dc63463dd81b2eb4910f7bda348@108.160.142.71:30301",    //JAPAN
+	"enode://e646471531c627e0b74a8ff15967c7d3f982c99883098d4b6921e3cc184232549d159dd682a08d59b58fdf864aa7966163aa0f6d661a9e7ae2d3bcb8157f51a1@45.76.148.77:30301",      //SINGAPORE
+        "enode://d6859aed0e935fe1c79c2b914b1b3981ff44d2a350997ff5a92fe57b996d01d8b7afe74237d39274937045a41f97b4fb71d0051f046e5541819c51dec1fe8907@45.76.117.164:30301",     //AUSTALIA
 	}
 
 // TestnetBootnodes are the enode URLs of the P2P bootstrap nodes running on the
