d547f7bc22790c44c1d17f935d2e4dc8000ed5c7
E1Code <39209383+E1Code@users.noreply.github.com>
2018-05-20 10:04:20 -0700

    Update config.go

--- eth/config.go
+++ eth/config.go
@@ -43,7 +43,7 @@ var DefaultConfig = Config{
 		DatasetsInMem:  1,
 		DatasetsOnDisk: 2,
 	},
-	NetworkId:     131142,
+	NetworkId:     113114,
 	LightPeers:    100,
 	DatabaseCache: 768,
 	TrieCache:     256,
