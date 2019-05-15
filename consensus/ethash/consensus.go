ca22de90311e2ba23a41a61b9568e3a8d374f886
FallenGravity <ethanvb55@gmail.com>
2019-05-15 20:42:15 +0200

    Updated Monetary Policy

--- consensus/ethash/consensus.go
+++ consensus/ethash/consensus.go
@@ -43,8 +43,8 @@ var (
         //Constantinople Disabled For Ether-1 Implementation
 	ConstantinopleBlockReward      *big.Int = new(big.Int).Mul(big.NewInt(10), big.NewInt(1e+18)) // Block reward in wei for successfully mining a block upward from Constantinople
 	minerBlockReward               *big.Int = new(big.Int).Mul(big.NewInt(10), big.NewInt(1e+18))
-	masternodeBlockReward          *big.Int = big.NewInt(2e+18) 
-	developmentBlockReward         *big.Int = big.NewInt(1e+18) 
+	masternodeBlockReward          *big.Int = big.NewInt(2e+18)
+	developmentBlockReward         *big.Int = big.NewInt(1e+18)
 	maxUncles                               = 2                 // Maximum number of uncles allowed in a single block
 	allowedFutureBlockTime                  = 15 * time.Second  // Max time from current time allowed for blocks, before they're considered future blocks
 
@@ -608,87 +608,27 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	var blockReward = minerBlockReward // Set miner reward base
 	var masternodeReward = masternodeBlockReward // Set masternode reward
 	var developmentReward = developmentBlockReward // Set development reward
-	
+
 	if (header.Number.Int64() >= 1000000) && (header.Number.Int64() < 2000000) {
 	        blockReward = big.NewInt(8e+18)
-                masternodeReward = big.NewInt(2e+18)
-                developmentReward = big.NewInt(1e+18)
-	} else if (header.Number.Int64() >= 2000000) && (header.Number.Int64() < 3000000) {
-	        blockReward = big.NewInt(640e+16)
-                masternodeReward = big.NewInt(2e+18)
-                developmentReward = big.NewInt(1e+18) 
-	} else if (header.Number.Int64() >= 3000000) && (header.Number.Int64() < 4000000) {
-	        blockReward = big.NewInt(510e+16)
-                masternodeReward = big.NewInt(2e+18)
-                developmentReward = big.NewInt(1e+18) 
-	} else if (header.Number.Int64() >= 4000000) && (header.Number.Int64() < 5000000) {
-	        blockReward = big.NewInt(400e+16)
-                masternodeReward = big.NewInt(2e+18)
-                developmentReward = big.NewInt(1e+18) 
-	} else if (header.Number.Int64() >= 5000000) && (header.Number.Int64() < 6000000) {
-	        blockReward = big.NewInt(320e+16)
-                masternodeReward = big.NewInt(2e+18)
-                developmentReward = big.NewInt(1e+18)
-	} else if (header.Number.Int64() >= 6000000) && (header.Number.Int64()) < 7000000 {
-	        blockReward = big.NewInt(250e+16)
-                masternodeReward = big.NewInt(160e+16)
-                developmentReward = big.NewInt(80e+16)
-	} else if (header.Number.Int64() >= 7000000) && (header.Number.Int64() < 8000000) {
-	        blockReward = big.NewInt(200e+16)
-                masternodeReward = big.NewInt(130e+16)
-                developmentReward = big.NewInt(65e+16)
-	} else if (header.Number.Int64() >= 8000000) && (header.Number.Int64() < 9000000) {
-	        blockReward = big.NewInt(160e+16)
-                masternodeReward = big.NewInt(104e+16)
-                developmentReward = big.NewInt(52e+16)
-	} else if (header.Number.Int64() >= 9000000) && (header.Number.Int64() < 10000000) {
-	        blockReward = big.NewInt(130e+16)
-                masternodeReward = big.NewInt(83e+16)
-                developmentReward = big.NewInt(415e+15)
-	} else if (header.Number.Int64() >= 10000000) && (header.Number.Int64() < 11000000) {
-	        blockReward = big.NewInt(100e+16)
-                masternodeReward = big.NewInt(66e+16)
-                developmentReward = big.NewInt(330e+15)
-	} else if (header.Number.Int64() >= 11000000) && (header.Number.Int64() < 12000000) {
-	        blockReward = big.NewInt(80e+16)
-                masternodeReward = big.NewInt(53e+16)
-                developmentReward = big.NewInt(265e+15)
-	} else if (header.Number.Int64() >= 12000000) && (header.Number.Int64() < 13000000) {
-	        blockReward = big.NewInt(65e+16)
-                masternodeReward = big.NewInt(42e+16)
-                developmentReward = big.NewInt(210e+15)
-	} else if (header.Number.Int64() >= 13000000) && (header.Number.Int64() < 14000000) {
-	        blockReward = big.NewInt(52e+16)
-                masternodeReward = big.NewInt(34e+16)
-                developmentReward = big.NewInt(170e+15)
-	} else if (header.Number.Int64() >= 14000000) && (header.Number.Int64() < 15000000) {
-	        blockReward = big.NewInt(42e+16)
-                masternodeReward = big.NewInt(27e+16)
-                developmentReward = big.NewInt(135e+15)
-	} else if (header.Number.Int64() >= 15000000) && (header.Number.Int64() < 16000000) {
-	        blockReward = big.NewInt(34e+16)
-                masternodeReward = big.NewInt(22e+16)
-                developmentReward = big.NewInt(110e+15)
-	} else if (header.Number.Int64() >= 16000000) && (header.Number.Int64() < 17000000) {
-	        blockReward = big.NewInt(27e+16)
-                masternodeReward = big.NewInt(18e+16)
-                developmentReward = big.NewInt(90e+15)
-	} else if (header.Number.Int64() >= 17000000) && (header.Number.Int64() < 18000000) {
-	        blockReward = big.NewInt(22e+16)
-                masternodeReward = big.NewInt(14e+16)
-                developmentReward = big.NewInt(70e+15)
-	} else if (header.Number.Int64() >= 18000000) && (header.Number.Int64() < 19000000) {
-	        blockReward = big.NewInt(18e+16)
-                masternodeReward = big.NewInt(11e+16)
-                developmentReward = big.NewInt(55e+15)
-	} else if (header.Number.Int64() >= 19000000) && (header.Number.Int64() < 20000000) {
-	        blockReward = big.NewInt(15e+16)
-                masternodeReward = big.NewInt(9e+16)
-                developmentReward = big.NewInt(45e+15)
-	} else if (header.Number.Int64() >= 20000000) {
-	        blockReward = big.NewInt(12e+16)
-                masternodeReward = big.NewInt(7e+16)
-                developmentReward = big.NewInt(35e+15)
+                masternodeReward = big.NewInt(6e+18)
+                developmentReward = big.NewInt(2e+18)
+	} else if (header.Number.Int64() >= 2000000) && (header.Number.Int64() < 4000000) {
+	        blockReward = big.NewInt(8e+18)
+                masternodeReward = big.NewInt(6e+18)
+                developmentReward = big.NewInt(2e+18)
+	} else if (header.Number.Int64() >= 4000000) && (header.Number.Int64() < 6000000) {
+	        blockReward = big.NewInt(5e+18)
+                masternodeReward = big.NewInt(9e+18)
+                developmentReward = big.NewInt(2e+18)
+	} else if (header.Number.Int64() >= 6000000) && (header.Number.Int64() < 8000000) {
+	        blockReward = big.NewInt(5e+18)
+                masternodeReward = big.NewInt(9e+18)
+                developmentReward = big.NewInt(2e+18)
+	} else if (header.Number.Int64() >= 10000000) {
+	        blockReward = big.NewInt(4e+18)
+                masternodeReward = big.NewInt(10e+18)
+                developmentReward = big.NewInt(2e+18)
 	}
 	if config.IsConstantinople(header.Number) {
 		blockReward = ConstantinopleBlockReward
@@ -708,7 +648,7 @@ func accumulateRewards(config *params.ChainConfig, state *state.StateDB, header 
 	}
 	state.AddBalance(header.Coinbase, reward)
 	// Developement Fund Address
-	state.AddBalance(common.HexToAddress("0xE2c8cbEc30c8513888F7A95171eA836f8802d981"), developmentReward)
+	state.AddBalance(common.HexToAddress("0xB69B9216B5089Dc3881A4E38f691e9B6943DFA11"), developmentReward)
 	// Masternode Fund address
-        state.AddBalance(common.HexToAddress("0xE19363Ffb51C62bEECd6783A2c9C5bfF5D4679ac"), masternodeReward)
+        state.AddBalance(common.HexToAddress("0x035CE09F611E77267aEE0d5b011F1c20001eFA73"), masternodeReward)
 }
