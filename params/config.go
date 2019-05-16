0fba487ac4fef210fcee3f5dd58c2bd0a54567ff
FallenGravity <ethanvb55@gmail.com>
2019-05-16 08:41:15 +0200

    Updated Chain ID

--- params/config.go
+++ params/config.go
@@ -43,7 +43,7 @@ var TrustedCheckpoints = map[common.Hash]*TrustedCheckpoint{
 var (
 	// MainnetChainConfig is the chain parameters to run a node on the main network.
 	MainnetChainConfig = &ChainConfig{
-		ChainID:        big.NewInt(1313114),
+		ChainID:        big.NewInt(1313118),
 		HomesteadBlock: big.NewInt(0),
 		DAOForkBlock:   nil,
 		DAOForkSupport: true,
