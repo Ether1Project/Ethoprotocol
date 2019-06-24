f5a217366bf157852ca67a3d66ddd49b40816197
FallenGravity <ethan@ether1.org>
2019-06-24 09:31:49 +0200

    Updated min diff

--- core/genesis.go
+++ core/genesis.go
@@ -310,8 +310,8 @@ func DefaultGenesisBlock() *Genesis {
 		Nonce:      0x0000000000000042,
 		ExtraData:  hexutil.MustDecode("0x646563656e7472616c697a6564206279206e6174757265"),
 		GasLimit:   3141592,
-		Difficulty: big.NewInt(200000000000),
-                Alloc: nil,
+		Difficulty: big.NewInt(5000),
+    Alloc: nil,
 	}
 }
 
