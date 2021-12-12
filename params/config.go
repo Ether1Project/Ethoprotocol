2728a9b727c9abd5248f87303cd90e15a4a3375c
James Reilly <admin@ether1.org>
2021-12-12 11:56:49 -0800

    Update config.go

--- params/config.go
+++ params/config.go
@@ -398,7 +398,7 @@ func (c *ChainConfig) String() string {
 	default:
 		engine = "unknown"
 	}
-	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Robinhood: %v Constantinople: %v Petersburg: %v Istanbul: %v Muir Glacier: %v, Berlin: %v, London: %v, Engine: %v}",
+	return fmt.Sprintf("{ChainID: %v Homestead: %v DAO: %v DAOSupport: %v EIP150: %v EIP155: %v EIP158: %v Byzantium: %v Robinhood: %v Constantinople: %v Petersburg: %v Istanbul: %v Muir Glacier: %v, NewHorizon: %v, Berlin: %v, London: %v, Engine: %v}",
 		c.ChainID,
 		c.HomesteadBlock,
 		c.DAOForkBlock,
